dda27dbff6aa8854cdad937afaa85386c13ca7b8
Joseph Martin <se.josephmartin@gmail.com>
2024-07-02 00:50:39 +0800

    chore: refactor subNav test

--- test/unit/components/navigation/subNav.test.js
+++ test/unit/components/navigation/subNav.test.js
@@ -3,44 +3,37 @@ import { render, screen } from '@testing-library/vue';
 import SubNav from '@/components/navigation/SubNav.vue';
 import { describe } from 'vitest';
 
+const renderSubNav = (routerName) => {
+  render(SubNav, {
+    global: {
+      mocks: {
+        $route: {
+          name: routerName
+        }
+      },
+      stubs: {
+        FontAwesomeIcon: true
+      }
+    }
+  });
+};
+
 describe('SubNav', () => {
   describe('when user is on jobs page', () => {
     it('display a jobs count', () => {
-      const $route = {
-        name: 'JobResults'
-      };
+      const routerName = 'JobResults';
+      renderSubNav(routerName);
 
-      render(SubNav, {
-        global: {
-          mocks: {
-            $route
-          },
-          stubs: {
-            FontAwesomeIcon: true
-          }
-        }
-      });
       const jobCount = screen.getByText('20');
-
       expect(jobCount).toBeInTheDocument();
     });
   });
 
   describe('when user is not on jobs page', () => {
     it('does not display the jobs count', () => {
-      const $route = {
-        name: 'Home'
-      };
-      render(SubNav, {
-        global: {
-          mocks: {
-            $route
-          },
-          stubs: {
-            FontAwesomeIcon: true
-          }
-        }
-      });
+      const routerName = 'Home';
+      renderSubNav(routerName);
+
       const jobCount = screen.queryByText('20');
       expect(jobCount).not.toBeInTheDocument();
     });
