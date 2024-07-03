4a49cda0cd1ec1b28705fefec74b77cbcb9424e1
Joseph Martin <se.josephmartin@gmail.com>
2024-07-03 19:15:40 +0800

    make a request from json server

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -10,10 +10,22 @@
 
 <script>
 import JobListing from '@/components/JobResults/JobListing.vue';
+import axios from 'axios';
+
 export default {
   name: 'JobListings',
   components: {
     JobListing
+  },
+  data() {
+    return {
+      jobs: []
+    };
+  },
+  mounted() {
+    axios.get('http://localhost:3000/jobs').then((response) => {
+      this.jobs = response.data;
+    });
   }
 };
 </script>
