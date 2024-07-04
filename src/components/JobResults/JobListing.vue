b49838d882f8db3cb31b4f8cea3ae52bbfa54503
Joseph Martin <se.josephmartin@gmail.com>
2024-07-04 11:33:25 +0800

    update joblisting to render job's data

--- src/components/JobResults/JobListing.vue
+++ src/components/JobResults/JobListing.vue
@@ -6,15 +6,25 @@
     >
       <div class="mx-8 border-solid border-brand-gray-2 pb-2 pt-5">
         <h2 class="mb-2 text-2xl">
-          Technical Program Manager, Perception, Augmented Reality
+          {{ job.title }}
         </h2>
 
         <div class="flex flex-row align-middle">
           <div class="mr-5">
-            <span>JobQuest</span>
+            <span>{{ job.organization }}</span>
           </div>
 
-          <div>San Francisco, CA, USA</div>
+          <div>
+            <ul>
+              <li
+                v-for="location in job.locations"
+                :key="location"
+                class="mr-5 inline-block"
+              >
+                <span> {{ location }}</span>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
 
@@ -23,11 +33,11 @@
           <h3 class="m-1 mb-2">Qualifications:</h3>
           <div>
             <ul class="list-disc pl-8">
-              <li>Bachelor's Degree or equivalent practical experience.</li>
-              <li>5 years of experience in program management</li>
-              <li>
-                Experience analyzing data through querying database (e.g., SQL)
-                using spreadsheet software, and creating statistical models.
+              <li
+                v-for="qualification in job.minimumQualifications"
+                :key="qualification"
+              >
+                {{ qualification }}
               </li>
             </ul>
           </div>
