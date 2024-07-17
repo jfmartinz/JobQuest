730ca410d1c8812d165d453ce65d9dc171b045c9
Joseph Martin <se.josephmartin@gmail.com>
2024-07-17 13:59:19 +0800

    feat: previous and next page

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -7,6 +7,30 @@
         :job="job"
       />
     </ol>
+
+    <div class="mx-auto mt-8">
+      <div class="flex flex-row flex-nowrap">     
+        <p class="flex-grow text-sm">Page {{ currentPage }}</p>
+        <div class="flex items-center justify-center">
+          <router-link 
+          v-if="previousPage"
+          class="mx-3 text-sm font-semibold text-brand-blue-1"
+          :to="{name: 'JobResults', query:{page: previousPage}}"
+          >
+          Previous
+      </router-link>
+      <router-link 
+          v-if="nextPage" 
+          class="mx-3 text-sm font-semibold text-brand-blue-1" 
+          :to="{name: 'JobResults', query:{page: nextPage}}"
+          >
+          Next Page
+      </router-link>
+    </div>
+      </div>
+      
+    </div>
+   
   </main>
 </template>
 
@@ -25,9 +49,23 @@ export default {
     };
   },
   computed: {
+    previousPage(){
+      const previousPage = this.currentPage - 1;
+      const firstPage = 1;
+
+      return previousPage >= firstPage ? previousPage : undefined;
+    },
+    nextPage(){
+      const nextPage = this.currentPage + 1;
+      const lastPage = this.jobs.length / 10;
+
+      return nextPage <= lastPage ? nextPage : undefined;
+    },
+    currentPage(){
+    return Number.parseInt(this.$route.query.page || '1')
+  },
     displayedJobs() {
-      const pageStr = this.$route.query.page || '1';
-      const pageNum = Number.parseInt(pageStr);
+      const pageNum = this.currentPage;
       const firstJobIndex = (pageNum - 1) * 10;
       const lastJobIndex = pageNum * 10;
       return this.jobs.slice(firstJobIndex, lastJobIndex);
