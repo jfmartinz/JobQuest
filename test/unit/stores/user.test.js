3c8286da6bd1917ec30b17f5a58f4e51314025a9
Joseph Martin <se.josephmartin@gmail.com>
2024-07-23 14:50:01 +0800

    fix active pinia state error

--- test/unit/stores/user.test.js
+++ test/unit/stores/user.test.js
@@ -1,20 +1,24 @@
 import { createPinia, setActivePinia } from 'pinia';
 
 import { useUserStore } from '@/stores/user';
-beforeEach(() => {
-  setActivePinia(createPinia());
-});
-describe('state', () => {
-  it('keeps track of if user is logged in', () => {
-    const store = useUserStore();
-    expect(store.isLoggedIn).toBe(false);
-  });
-});
 
-describe('action', () => {
-  it('logs the user in', () => {
-    const store = useUserStore();
-    store.loginUser();
-    expect(store.isLoggedIn).toBe(true);
+describe('User Store', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  describe('state', () => {
+    it('keeps track of if user is logged in', () => {
+      const store = useUserStore();
+      expect(store.isLoggedIn).toBe(false);
+    });
+  });
+
+  describe('action', () => {
+    it('logs the user in', () => {
+      const store = useUserStore();
+      store.loginUser();
+      expect(store.isLoggedIn).toBe(true);
+    });
   });
 });
