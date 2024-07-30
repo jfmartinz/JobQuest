26586e75d81f0710cfe6cd923eb7f03fe29c46e2
Joseph Martin <se.josephmartin@gmail.com>
2024-07-30 13:19:24 +0800

    fix(tests): provide Pinia store in JobListings unit test

--- test/unit/components/jobResults/JobListings.test.js
+++ test/unit/components/jobResults/JobListings.test.js
@@ -2,30 +2,34 @@ import { render, screen } from '@testing-library/vue';
 import { RouterLinkStub } from '@vue/test-utils';
 import axios from 'axios';
 import JobListings from '@/components/JobResults/JobListings.vue';
-
+import { createPinia, setActivePinia } from 'pinia';
 vi.mock('axios');
 
 describe('JobListings', () => {
-  const createRoute = (queryParams)=> ({
-    query:{
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  const createRoute = (queryParams) => ({
+    query: {
       page: '5',
       ...queryParams
     }
-  })
+  });
 
   const renderJobListings = ($route) => {
     render(JobListings, {
       global: {
-        mocks:{
-          $route,
+        mocks: {
+          $route
         },
         stubs: {
           RouterLink: RouterLinkStub
         }
       }
     });
-  }
-   
+  };
+
   beforeEach(() => {
     // Mock the environment variable
     process.env.VITE_APP_API_URL = 'http://myfakeapi.com';
@@ -34,17 +38,17 @@ describe('JobListings', () => {
   it('fetches job', () => {
     axios.get.mockResolvedValue({ data: [] });
     const $route = createRoute();
-      
+
     renderJobListings($route);
 
     expect(axios.get).toHaveBeenCalledWith('http://myfakeapi.com/jobs');
   });
-  
+
   it('displays a maximum of 10 jobs', async () => {
     axios.get.mockResolvedValue({ data: Array(15).fill({}) });
-    const queryParams = {page: '1'};
+    const queryParams = { page: '1' };
     const $route = createRoute(queryParams);
-      
+
     renderJobListings($route);
 
     const jobListings = await screen.findAllByRole('listitem');
