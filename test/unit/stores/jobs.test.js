9ea1f6a9f2b1b9d243c330c9e51705d884fde1d9
Joseph Martin <se.josephmartin@gmail.com>
2024-07-30 09:59:32 +0800

    test: Fetches jobs and stores recieved jobs

--- test/unit/stores/jobs.test.js
+++ test/unit/stores/jobs.test.js
@@ -1,6 +1,8 @@
 import { createPinia, setActivePinia } from 'pinia';
-
+import axios from 'axios';
 import { useJobsStore } from '@/stores/jobs';
+
+vi.mock('axios');
 
 describe('State', () => {
   beforeEach(() => {
@@ -10,5 +12,14 @@ describe('State', () => {
   it('stores job listings', () => {
     const store = useJobsStore();
     expect(store.jobs).toEqual([]);
+  });
+
+  describe('FETCH_JOBS', () => {
+    it('makes an API request and stores received jobs', async () => {
+      axios.get.mockResolvedValue({ data: ['Jobs 1', 'Jobs 2'] });
+      const store = useJobsStore();
+      await store.FETCH_JOBS();
+      expect(store.jobs).toEqual(['Jobs 1', 'Jobs 2']);
+    });
   });
 });
