24474ea69ef82e5750bfe93c997d780d52ecc084
Joseph Martin <se.josephmartin@gmail.com>
2024-08-01 20:43:38 +0800

    Add two new icons from fontawesome

--- src/main.js
+++ src/main.js
@@ -10,9 +10,15 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import {
+  faSearch,
+  faAngleDown,
+  faAngleUp
+} from '@fortawesome/free-solid-svg-icons';
 
 library.add(faSearch);
+library.add(faAngleDown);
+library.add(faAngleUp);
 
 const pinia = createPinia();
 
