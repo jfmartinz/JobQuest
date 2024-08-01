507403bedff0ced77b26dbf5d2914e650fa10dd1
Joseph Martin <se.josephmartin@gmail.com>
2024-08-01 20:39:31 +0800

    Add styles clear filters

--- src/components/JobResults/JobsFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobsFiltersSidebar/JobFiltersSidebar.vue
@@ -1,9 +1,19 @@
 <template>
-  <h1
-    class="flex w-96 flex-col border-r border-solid border-brand-gray-1 bg-white p-4"
+  <div
+    class="border-brand border-solid-gray-1 flex w-96 flex-col border-r bg-white p-4"
   >
-    Job Filters Sidebar
-  </h1>
+    <section class="pb-5">
+      <div class="flex flex-row justify-between">
+        <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
+        <div class="flex items-center text-sm">
+          <action-button
+            text="Clear Filters"
+            type="secondary"
+          ></action-button>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
