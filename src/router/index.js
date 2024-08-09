632d1b5b3fe70bbaa839d9985d4e441f873d2b15
Joseph Martin <se.josephmartin@gmail.com>
2024-08-09 11:33:00 +0800

    Add TeamsView in routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 
 import HomeView from '@/views/HomeView.vue';
 import JobResultsView from '@/views/JobResultsView.vue';
+import TeamsView from '@/views/TeamsView.vue';
 
 const routes = [
   {
@@ -13,14 +14,19 @@ const routes = [
     path: '/jobs/results',
     name: 'JobResults',
     component: JobResultsView
+  },
+  {
+    path: '/teams/',
+    name: 'Teams',
+    component: TeamsView
   }
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
-  scrollBehavior(){
-    return{top: 0, left: 0, behavior: 'smooth'};
+  scrollBehavior() {
+    return { top: 0, left: 0, behavior: 'smooth' };
   }
 });
 
