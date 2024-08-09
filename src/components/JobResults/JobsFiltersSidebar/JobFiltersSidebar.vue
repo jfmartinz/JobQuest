89e82c043392ade59769a11163b3f07ec0418e44
Joseph Martin <se.josephmartin@gmail.com>
2024-08-09 08:24:47 +0800

    Add content orgs to CollapsibleAccordion

--- src/components/JobResults/JobsFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobsFiltersSidebar/JobFiltersSidebar.vue
@@ -13,7 +13,46 @@
         </div>
       </div>
 
-      <collapsible-accordion> Hello Custom Component </collapsible-accordion>
+      <collapsible-accordion>
+        <div class="mt-5">
+          <fieldset>
+            <ul class="flex flex-row flex-wrap">
+              <li class="h-8 w-1/2">
+                <input
+                  id="VueTube"
+                  type="checkbox"
+                  class="mr-3"
+                />
+                <label for="VueTube">VueTube</label>
+              </li>
+              <li class="h-8 w-1/2">
+                <input
+                  id="Between Vue"
+                  type="checkbox"
+                  class="mr-3"
+                />
+                <label for="BetweenVue">Between Vue</label>
+              </li>
+              <li class="h-8 w-1/2">
+                <input
+                  id="Et"
+                  type="checkbox"
+                  class="mr-3"
+                />
+                <label for="Et Vue Brute">Et Vue Brute</label>
+              </li>
+              <li class="h-8 w-1/2">
+                <input
+                  id="Vue and a Half Men"
+                  type="checkbox"
+                  class="mr-3"
+                />
+                <label for="Vue and a Half Men">Vue and a Half Men</label>
+              </li>
+            </ul>
+          </fieldset>
+        </div>
+      </collapsible-accordion>
     </section>
   </div>
 </template>
