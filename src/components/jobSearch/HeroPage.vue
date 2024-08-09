e32d3fa8c8012a05f9c59e4509939075fc86e1c1
Joseph Martin <se.josephmartin@gmail.com>
2024-08-09 14:37:29 +0800

    Render SpotLight component

--- src/components/jobSearch/HeroPage.vue
+++ src/components/jobSearch/HeroPage.vue
@@ -17,18 +17,46 @@
         <div class="col-span-1 col-start-12"></div>
       </div>
     </section>
+    <spot-light class="flex flex-row justify-center pb-16">
+      <template #default="slotProps">
+        <router-link
+          to="/jobs/results"
+          class="mx-5 flex h-96 w-72 flex-col rounded-lg border bg-brand-gray-2"
+        >
+          <img
+            :src="slotProps.spotlight.img"
+            alt=""
+            class="object-contain"
+          />
+
+          <div class="mt-3 h-48 px-6 py-4">
+            <h3 class="text-lg font-medium">{{ slotProps.spotlight.title }}</h3>
+            <p class="mt-3 text-sm">
+              {{ slotProps.spotlight.description }}
+            </p>
+          </div>
+          <router-link
+            to="/jobs/results"
+            class="px-6 pb-4 text-sm text-brand-blue-1"
+            >See jobs</router-link
+          >
+        </router-link>
+      </template>
+    </spot-light>
   </main>
 </template>
 
 <script>
 import HeadLine from '@/components/JobSearch/HeadLine.vue';
 import JobSearchForm from '@/components/JobSearch/JobSearchForm.vue';
+import SpotLight from '@/components/JobSearch/SpotLight.vue';
 
 export default {
   name: 'HeroPage',
   components: {
     HeadLine,
-    JobSearchForm
+    JobSearchForm,
+    SpotLight
   },
   data() {
     return {};
