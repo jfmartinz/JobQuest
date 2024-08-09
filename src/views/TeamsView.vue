2f3b53d7b26c62cbb4503122047e2f08d0e8c793
Joseph Martin <se.josephmartin@gmail.com>
2024-08-09 11:33:16 +0800

    Add TeamsView component

---
+++ src/views/TeamsView.vue
@@ -0,0 +1,13 @@
+<template>
+  <header-container />
+</template>
+
+<script>
+import HeaderContainer from '@/components/HeaderContainer.vue';
+export default {
+  name: 'TeamsView',
+  components: {
+    HeaderContainer
+  }
+};
+</script>
