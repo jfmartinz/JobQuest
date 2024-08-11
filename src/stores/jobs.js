b69865aa3a34172b7324632771997f0792b5ccef
Joseph Martin <se.josephmartin@gmail.com>
2024-08-11 10:52:17 +0800

    Add pinia getters

--- src/stores/jobs.js
+++ src/stores/jobs.js
@@ -3,6 +3,7 @@ import { defineStore } from 'pinia';
 import getJobs from '@/api/getJobs';
 
 export const FETCH_JOBS = 'FETCH_JOBS';
+export const UNIQUE_ORGANIZATION = 'UNIQUE_ORGANIZATION';
 
 export const useJobsStore = defineStore('jobs', {
   state: () => ({
@@ -12,6 +13,13 @@ export const useJobsStore = defineStore('jobs', {
     async [FETCH_JOBS]() {
       const jobs = await getJobs();
       this.jobs = jobs;
+    }
+  },
+  getters: {
+    [UNIQUE_ORGANIZATION](state) {
+      const uniqueOrganizations = new Set();
+      state.jobs.forEach((job) => uniqueOrganizations.add(job.organization));
+      return uniqueOrganizations;
     }
   }
 });
