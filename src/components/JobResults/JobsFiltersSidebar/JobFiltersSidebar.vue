2bda5d23a6f39eef710b7e322114e9b8afb79f66
Joseph Martin <se.josephmartin@gmail.com>
2024-08-12 15:03:28 +0800

    Update JobFilterSidebar component

--- src/components/JobResults/JobsFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobsFiltersSidebar/JobFiltersSidebar.vue
@@ -12,44 +12,20 @@
           ></action-button>
         </div>
       </div>
-      <collapsible-accordion header="Organizations">
-        <div class="mt-5">
-          <fieldset>
-            <ul class="flex flex-row flex-wrap">
-              <li
-                v-for="organization in UNIQUE_ORGANIZATION"
-                :key="organization"
-                class="h-8 w-1/2"
-              >
-                <input
-                  :id="organization"
-                  type="checkbox"
-                  class="mr-3"
-                />
-                <label :for="organization">{{ organization }}</label>
-              </li>
-            </ul>
-          </fieldset>
-        </div>
-      </collapsible-accordion>
+      <job-filters-sidebar-organizations></job-filters-sidebar-organizations>
     </section>
   </div>
 </template>
 
 <script>
 import ActionButton from '@/components/Navigation/ActionButton.vue';
-import CollapsibleAccordion from '@/components/CollapsibleAccordion.vue';
+import JobFiltersSidebarOrganizations from './JobFiltersSidebarOrganizations.vue';
 
-import { mapState } from 'pinia';
-import { useJobsStore, UNIQUE_ORGANIZATION } from '@/stores/jobs';
 export default {
   name: 'JobFiltersSidebar',
   components: {
     ActionButton,
-    CollapsibleAccordion
-  },
-  computed: {
-    ...mapState(useJobsStore, [UNIQUE_ORGANIZATION])
+    JobFiltersSidebarOrganizations
   }
 };
 </script>
