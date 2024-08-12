5321a8e0ed9d2235af040c86a3d4ecd596aeb248
Joseph Martin <se.josephmartin@gmail.com>
2024-08-12 15:04:09 +0800

    Add JobFiltersSidebarOrganizations component

---
+++ src/components/JobResults/JobsFiltersSidebar/JobFiltersSidebarOrganizations.vue
@@ -0,0 +1,37 @@
+<template>
+  <collapsible-accordion header="Organizations">
+    <div class="mt-5">
+      <fieldset>
+        <ul class="flex flex-row flex-wrap">
+          <li
+            v-for="organization in UNIQUE_ORGANIZATION"
+            :key="organization"
+            class="h-8 w-1/2"
+          >
+            <input
+              :id="organization"
+              type="checkbox"
+              class="mr-3"
+            />
+            <label :for="organization">{{ organization }}</label>
+          </li>
+        </ul>
+      </fieldset>
+    </div>
+  </collapsible-accordion>
+</template>
+
+<script>
+import { mapState } from 'pinia';
+import { useJobsStore, UNIQUE_ORGANIZATION } from '@/stores/jobs';
+import CollapsibleAccordion from '@/components/CollapsibleAccordion.vue';
+export default {
+  name: 'JobFiltersSidebarOrganizations',
+  components: {
+    CollapsibleAccordion
+  },
+  computed: {
+    ...mapState(useJobsStore, [UNIQUE_ORGANIZATION])
+  }
+};
+</script>
