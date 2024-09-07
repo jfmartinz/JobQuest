7b930248fe22cdcddefa98e9f9d09db9b8501e28
Joseph Martin <se.josephmartin@gmail.com>
2024-09-07 20:33:53 +0800

    add FILTERED_JOBS_BY_ORGANIZATION getter

--- src/stores/jobs.js
+++ src/stores/jobs.js
@@ -1,9 +1,10 @@
 import { defineStore } from 'pinia';
 
 import getJobs from '@/api/getJobs';
-
+import { useUserStore } from '@/stores/user';
 export const FETCH_JOBS = 'FETCH_JOBS';
 export const UNIQUE_ORGANIZATION = 'UNIQUE_ORGANIZATION';
+export const FILTERED_JOBS_BY_ORGANIZATION = 'FILTERED_JOBS_BY_ORGANIZATION';
 
 export const useJobsStore = defineStore('jobs', {
   state: () => ({
@@ -20,6 +21,12 @@ export const useJobsStore = defineStore('jobs', {
       const uniqueOrganizations = new Set();
       state.jobs.forEach((job) => uniqueOrganizations.add(job.organization));
       return uniqueOrganizations;
+    },
+    [FILTERED_JOBS_BY_ORGANIZATION](state) {
+      const userStore = useUserStore();
+      return state.jobs.filter((job) =>
+        userStore.selectedOrganizations.includes(job.organization)
+      );
     }
   }
 });
