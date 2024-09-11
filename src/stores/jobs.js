ebe0004d4b8de8db22912288dd29bb64958491a6
Joseph Martin <se.josephmartin@gmail.com>
2024-09-11 20:45:41 +0800

    add UNIQUE_JOB_TYPES getter in jobs store

--- src/stores/jobs.js
+++ src/stores/jobs.js
@@ -5,6 +5,7 @@ import { useUserStore } from '@/stores/user';
 export const FETCH_JOBS = 'FETCH_JOBS';
 export const UNIQUE_ORGANIZATION = 'UNIQUE_ORGANIZATION';
 export const FILTERED_JOBS_BY_ORGANIZATION = 'FILTERED_JOBS_BY_ORGANIZATION';
+export const UNIQUE_JOB_TYPES = 'UNIQUE_JOB_TYPES';
 
 export const useJobsStore = defineStore('jobs', {
   state: () => ({
@@ -21,6 +22,11 @@ export const useJobsStore = defineStore('jobs', {
       const uniqueOrganizations = new Set();
       state.jobs.forEach((job) => uniqueOrganizations.add(job.organization));
       return uniqueOrganizations;
+    },
+    [UNIQUE_JOB_TYPES](state) {
+      const uniqueJobTypes = new Set();
+      state.jobs.forEach((job) => uniqueJobTypes.add(job.jobType));
+      return uniqueJobTypes;
     },
     [FILTERED_JOBS_BY_ORGANIZATION](state) {
       const userStore = useUserStore();
