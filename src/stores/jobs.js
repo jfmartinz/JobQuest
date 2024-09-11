2f29aeac4ecc8218e92245b2804d08473facaf5f
Joseph Martin <se.josephmartin@gmail.com>
2024-09-11 20:55:18 +0800

    add FILTERED_JOBS_JOB_TYPES getter in jobs store

--- src/stores/jobs.js
+++ src/stores/jobs.js
@@ -6,6 +6,7 @@ export const FETCH_JOBS = 'FETCH_JOBS';
 export const UNIQUE_ORGANIZATION = 'UNIQUE_ORGANIZATION';
 export const FILTERED_JOBS_BY_ORGANIZATION = 'FILTERED_JOBS_BY_ORGANIZATION';
 export const UNIQUE_JOB_TYPES = 'UNIQUE_JOB_TYPES';
+export const FILTERED_JOBS_BY_JOB_TYPES = 'FILTERED_JOBS_BY_JOB_TYPES';
 
 export const useJobsStore = defineStore('jobs', {
   state: () => ({
@@ -27,6 +28,15 @@ export const useJobsStore = defineStore('jobs', {
       const uniqueJobTypes = new Set();
       state.jobs.forEach((job) => uniqueJobTypes.add(job.jobType));
       return uniqueJobTypes;
+    },
+    [FILTERED_JOBS_BY_JOB_TYPES](state) {
+      const userStore = useUserStore();
+      if (userStore.selectedJobTypes.length === 0) {
+        return state.jobs;
+      }
+      return state.jobs.filter((job) =>
+        userStore.selectedJobTypes.includes(job.jobType)
+      );
     },
     [FILTERED_JOBS_BY_ORGANIZATION](state) {
       const userStore = useUserStore();
