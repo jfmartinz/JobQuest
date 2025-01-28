1566b6a5f0f81841607d5affad30c07969328d9b
Joseph Martin <se.josephmartin@gmail.com>
2025-01-28 19:35:26 +0800

    update utils file to typescript

--- src/utils/nextElementInList.js
+++
@@ -1,8 +0,0 @@
-const nextElementInList = (lists, value) => {
-  const currentValueIndex = lists.indexOf(value)
-  const nextVerbIndex = (currentValueIndex + 1) % lists.length
-  const nextValue = lists[nextVerbIndex]
-  return nextValue
-}
-
-export default nextElementInList
